639a1139c8182897db4577d19a298e4bcddc6a12
Century178 <mrothenberger0918@gmail.com>
2023-02-09 19:35:54 -0700

    Better ground

---
+++ Assets/Scripts/Ground.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ground : MonoBehaviour
+{
+    [SerializeField] public float scrollSpeed;
+    [SerializeField] public float offset;
+    [SerializeField] public Transform ground;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(scrollSpeed * Time.deltaTime, 0, 0);
+    }
+
+    void OnTriggerExit2D(Collider2D collision)
+    {
+        transform.position = new Vector3(ground.transform.position.x + offset, ground.transform.position.y, ground.transform.position.z);
+    }
+}
