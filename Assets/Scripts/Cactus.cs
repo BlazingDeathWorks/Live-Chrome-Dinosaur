35f7396886569f4f6c8a48433ab0fc85400d5c7e
Jason Chan <shunsei.chan@gmail.com>
2023-02-11 17:32:37 -0800

    Everything fixed

--- Assets/Scripts/Cactus.cs
+++ Assets/Scripts/Cactus.cs
@@ -13,15 +13,6 @@ public class Cactus : MonoBehaviour
         rb = GetComponent<Rigidbody2D>();
     }
 
-    //Detect collision with player
-    private void OnTriggerEnter2D(Collider2D collision)
-    {
-        if (collision.gameObject.CompareTag("Player"))
-        {
-
-        }
-    }
-
     private void FixedUpdate()
     {
         rb.velocity = Vector2.left * speed;
--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -6,19 +6,19 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     public float jumpPower = 1;
+    public Sprite standSprite, duckSprite;
+    public GameObject button;
+
     private Rigidbody2D rb;
-    private CircleCollider2D groundTrigger;
     private BoxCollider2D hitbox;
     private bool canJump, isJumping;
     private SpriteRenderer render;
-    public Sprite standSprite, duckSprite;
-
-    public GameObject button;
+    private bool isDead = false;
 
     private void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
-        groundTrigger = GetComponent<CircleCollider2D>();
+        render = GetComponent<SpriteRenderer>();
         button.SetActive(false);
         Time.timeScale = 1f;
         hitbox = GetComponent<BoxCollider2D>();
@@ -26,6 +26,8 @@ public class PlayerMovement : MonoBehaviour
 
     private void Update()
     {
+        if (isDead) return;
+
         if (Input.GetKeyDown(KeyCode.Space) && canJump &!Input.GetKey(KeyCode.LeftShift))
         {
             isJumping = true;
@@ -34,11 +36,13 @@ public class PlayerMovement : MonoBehaviour
         if (Input.GetKey(KeyCode.LeftShift) &! Input.GetKeyDown(KeyCode.Space))
         {
             hitbox.size = new Vector2(0.8f, 0.45f);
+            //hitbox.offset = new Vector2(0, -0.225f);
             render.sprite = duckSprite;
         }
         else
         {
             hitbox.size = new Vector2(0.8f, 0.9f);
+            //hitbox.offset = Vector2.zero;
             render.sprite = standSprite;
         }
     }
@@ -53,15 +57,22 @@ public class PlayerMovement : MonoBehaviour
         }
     }
 
-    private void OnTriggerEnter2D(Collider2D collision)
+    private void OnCollisionEnter2D(Collision2D collision)
     {
-        if (collision.gameObject.CompareTag("Ground")) canJump = true;
-
         if (collision.gameObject.CompareTag("Cactus"))
         {
             Time.timeScale = 0f;
+            isDead = true;
 
             button.SetActive(true);
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            canJump = true;
         }
     }
 }
